1c43c6cd724a3f1573aa1f6fc253a9a5319da6d3
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-05-05 21:29:26 +0100

    feat: create get_show_image

--- src/trakt.rs
+++ src/trakt.rs
@@ -1,6 +1,8 @@
 use serde::Deserialize;
 use std::{collections::HashMap, time::Duration};
-use ureq::{Agent, AgentBuilder};
+use ureq::{serde_json, Agent, AgentBuilder};
+
+use crate::utils::log;
 
 #[derive(Deserialize)]
 pub struct TraktMovie {
@@ -53,7 +55,8 @@ pub struct TraktRatingsResponse {
 }
 
 pub struct Trakt {
-    cache: HashMap<String, f64>,
+    rating_cache: HashMap<String, f64>,
+    image_cache: HashMap<String, String>,
     agent: Agent,
     client_id: String,
     username: String,
@@ -62,7 +65,8 @@ pub struct Trakt {
 impl Trakt {
     pub fn new(client_id: String, username: String) -> Trakt {
         Trakt {
-            cache: HashMap::default(),
+            rating_cache: HashMap::default(),
+            image_cache: HashMap::default(),
             agent: AgentBuilder::new()
                 .timeout_read(Duration::from_secs(5))
                 .timeout_write(Duration::from_secs(5))
@@ -93,9 +97,46 @@ impl Trakt {
         }
     }
 
-    pub fn get_movie_rating(&mut self, movie_slug: String) -> Option<f64> {
-        match self.cache.get(&movie_slug) {
-            Some(rating) => Some(*rating),
+    pub fn get_show_image(&mut self, imdb_id: String) -> Option<String> {
+        match self.image_cache.get(&imdb_id) {
+            Some(image_url) => Some(image_url.to_string()),
+            None => {
+                let endpoint = format!("https://imdb-api.com/en/API/Posters/k_jiv7uc8t/{imdb_id}");
+
+                let response = match self.agent.get(&endpoint).call() {
+                    Ok(response) => response,
+                    Err(_) => {
+                        log("Failed to get image from imdb-api");
+                        return None;
+                    }
+                };
+
+                match response.into_json::<serde_json::Value>() {
+                    Ok(body) => {
+                        let image_url = body["posters"][0]["link"]
+                            .to_string()
+                            .replace("original", "s470");
+
+                        if image_url == "null" {
+                            log("Failed to extract image from imdb-api");
+                            return None;
+                        }
+
+                        self.image_cache.insert(imdb_id, image_url.to_string());
+                        Some(image_url)
+                    }
+                    Err(_) => {
+                        log("Show image not correctly found");
+                        return None;
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn get_movie_rating(&mut self, movie_slug: String) -> f64 {
+        match self.rating_cache.get(&movie_slug) {
+            Some(rating) => *rating,
             None => {
                 let endpoint = format!("https://api.trakt.tv/movies/{}/ratings", movie_slug);
 
@@ -108,16 +149,16 @@ impl Trakt {
                     .call()
                 {
                     Ok(response) => response,
-                    Err(_) => return Some(0.0),
+                    Err(_) => return 0.0,
                 };
 
-                match response.into_json() {
+                match response.into_json::<TraktRatingsResponse>() {
                     Ok(body) => {
-                        let body: TraktRatingsResponse = body;
-                        self.cache.insert(movie_slug.to_string(), body.rating);
-                        Some(body.rating)
+                        self.rating_cache
+                            .insert(movie_slug.to_string(), body.rating);
+                        body.rating
                     }
-                    Err(_) => Some(0.0),
+                    Err(_) => 0.0,
                 }
             }
         }
